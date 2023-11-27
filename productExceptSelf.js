d7630ffbc4a6046334fe7cd9e3709544bc47cd0a
Aparna Prayaga <aparnaprayaga@Aparnas-MacBook-Pro.local>
2023-11-27 09:49:21 -0800

    quiz-1 solution

---
+++ productExceptSelf.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let leftArr = [];
+    let rightArr = [];
+    let product;
+
+    leftArr[0] = 1;
+    product = 1;
+
+    for(let i = 1; i <= nums.length - 1; i++){
+        product = product * nums[i - 1];
+        leftArr[i] = product;
+    }
+
+    rightArr[nums.length - 1] = 1;
+    product = 1;
+
+    for(let i = nums.length - 2; i >= 0; i--){
+        product = product * nums[i + 1];
+        rightArr[i] = product;
+        
+    }
+
+    let resultArr = [];
+    for (let k = 0; k < nums.length; k++){
+        resultArr[k] = leftArr[k] * rightArr[k];
+    }
+
+    return resultArr;
+};
