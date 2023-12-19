cb2b1ac03ee7896eda6842c0f0859936e9906c24
Aparna Prayaga <aparnaprayaga@Aparnas-MacBook-Pro.local>
2023-12-18 21:14:08 -0800

    longest common prefix

---
+++ longestCommonPrefix.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let prefix = strs.reduce((acc, str) => str.length < acc.length ? str : acc);
+    
+    for (let str of strs) {
+        while (str.slice(0, prefix.length) != prefix) {
+            prefix = prefix.slice(0, -1);
+        }
+    }
+    return prefix;
+};
+
+const strs = ["flower","flow","flight"];
+// const strs = ["dog","racecar","car"];
+console.log(longestCommonPrefix(strs));
