fc440c19553e133a63b4d16f15b92ce45286cf4f
Aparna Prayaga <aparnaprayaga@Aparnas-MacBook-Pro.local>
2024-01-03 17:15:13 -0800

    gas station problem

---
+++ gasStationProblem.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function(gas, cost) {
+    let curTank = 0, totalTank = 0, position = 0;
+    for (let i=0;i<gas.length;i++) {
+        curTank+= gas[i] - cost[i];
+        totalTank+= gas[i] - cost[i];
+        if (curTank<0) {
+            curTank = 0;
+            position = i+1;
+        }
+    }   
+    return totalTank<0?-1:position;
+
+};
