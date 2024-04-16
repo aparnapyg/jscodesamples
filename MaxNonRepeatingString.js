8ab10824d82851726329e3f7a7df3aeab0b8dbbc
Aparna Prayaga <aparnaprayaga@Aparnas-MacBook-Pro.local>
2024-04-16 10:23:44 -0700

    Max non repeating sliding window update

--- MaxNonRepeatingString.js
+++ MaxNonRepeatingString.js
@@ -2,31 +2,26 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    let start = 0;
-    let end = 0;
-    maxLengthOfString = 0;
-
-    while (end < s.length){
-        while (start < s.length) {
-            lengthOfString = getLengthOfSubstrWithNoRepeatingChars(s.substring(start, end));
-            if (lengthOfString > maxLengthOfString) { maxLengthOfString = lengthOfString; }
-            start++;
-        }
-        end++;
-    }
-    return maxLengthOfString;
-};
-
-function getLengthOfSubstrWithNoRepeatingChars(str){
-    for(let i = 0; i < str.length; i++){
-        for(let j = i+1; j < str.length; j++){
-            if (str.charAt(i) === str.charAt(j))  { return true; }
-        }
-    }
-    return str.length;
+ var lengthOfLongestSubstring = function(s) {
+ const setMap = new Set();
+ let max = 0;
+ let index = 0;
+ for(let i = 0; i<s.length; i++){
+     let key = s[i];
+     while(setMap.has(key)){
+         setMap.delete(s[index]);
+         index++;
+     }
+     setMap.add(key, i);
+     max = Math.max(max, setMap.size);
+ }
+ return max;
 }
 
-const s = 'abcabcbb';
+// const s = 'abcabcbb';
+
+// const s = 'bbbb';
+
+const s = 'pwwkew';
 
 console.log(lengthOfLongestSubstring(s));
